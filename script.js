849b45578e6f6c8d22bc160a2734f8acf6b1b549
Avi Fenesh <aviarchi1994@gmail.com>
2021-07-31 20:08:18 +0300

    add usage of local storage

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let ul = document.getElementById("list");
 let li;
+let itemNum = 0;
 // create event for buttons
 let addButton = document.getElementById("add");
 addButton.addEventListener("click", addItem);
@@ -9,6 +10,30 @@ removeButton.addEventListener("click", removeItem);
 
 let removeAllButton = document.getElementById("remove-all");
 removeAllButton.addEventListener("click", removeAllItem);
+
+// adding items from local storage
+function addElementFls(item) {
+  let input = document.getElementById("input");
+  ul = document.getElementById("list");
+  let textNode = document.createTextNode(item);
+  li = document.createElement("li");
+  let checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.setAttribute("id", "check");
+  let lable = document.createElement("label");
+  lable.setAttribute("for", "item");
+  ul.appendChild(lable);
+  li.appendChild(checkBox);
+  lable.appendChild(textNode);
+  li.appendChild(lable);
+  ul.insertBefore(li, ul.childNodes[0]);
+  li.className = "visual";
+  input.value = "";
+}
+//check if there is itens un the local storage and add them - for save todoes while refreshing
+for (let i = 0; i < localStorage.length; i++) {
+  addElementFls(localStorage.getItem(i + 1));
+}
 
 // get the value from the input text, if valid add new check box else return a message
 function addItem() {
@@ -40,6 +65,8 @@ function addItem() {
       li.className = "visual";
       input.value = "";
     }, 3); // for smoth felling
+    localStorage.setItem(itemNum + 1, item);
+    itemNum++;
   }
 }
 // look for all the checked items and clear them
@@ -49,6 +76,7 @@ function removeItem() {
     const element = li[index];
     while (li[index] && li[index].children[0].checked) {
       ul.removeChild(li[index]);
+      localStorage.removeItem(itemNum - index + 1); //clear from local storage
     }
   }
 }
@@ -59,4 +87,5 @@ function removeAllItem() {
   for (let index = 0; index < li.length; index++) {
     ul.removeChild(li[index]);
   }
+  localStorage.clear();
 }
